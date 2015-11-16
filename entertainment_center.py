22fef15d15b550c7b3ed8449ea1f13b886c6bab9
mrchristmas <mrchristmas@users.noreply.github.com>
2015-11-16 17:15:41 -0600

    Create entertainment_center.py

---
+++ entertainment_center.py
@@ -0,0 +1,63 @@
+import media
+import fresh_tomatoes
+
+#Create instances of Movie class
+goodfellas = media.Movie("Goodfellas",
+                         "1990",
+                         "Henry Hill and his friends work their way up through the mob hierarchy.",
+                         "https://upload.wikimedia.org/wikipedia/en/7/7b/Goodfellas.jpg",
+                         "https://www.youtube.com/watch?v=qo5jJpHtI1Y")
+                         
+usual_suspects = media.Movie("The Usual Suspects",
+                             "1995",
+                             "A sole survivor tells of the twisty events leading up to a horrific gun battle on a boat, which begin when five criminals meet at a seemingly random police lineup.",
+                             "https://upload.wikimedia.org/wikipedia/en/9/9c/Usual_suspects_ver1.jpg",
+                             "https://www.youtube.com/watch?v=oiXdPolca5w")
+
+django_unchained = media.Movie("Django Unchained",
+                               "2012",
+                               "With the help of a German bounty hunter, a freed slave sets out to rescue his wife from a brutal Mississippi plantation owner.",
+                               "https://upload.wikimedia.org/wikipedia/en/8/8b/Django_Unchained_Poster.jpg",
+                               "https://www.youtube.com/watch?v=eUdM9vrCbow")
+
+reservoir_dogs = media.Movie("Reservoir Dogs",
+                             "1992",
+                             "After a simple jewelery heist goes terribly wrong, the surviving criminals begin to suspect that one of them is a police informant.",
+                             "https://upload.wikimedia.org/wikipedia/en/f/f6/Reservoir_dogs_ver1.jpg",
+                             "https://www.youtube.com/watch?v=GLPJSmUHZvU")
+                  
+home_alone = media.Movie("Home Alone",
+                         "1990",
+                         "An 8-year old troublemaker must protect his home from a pair of burglars when he is accidentally left home alone by his family during Christmas vacation.",
+                         "https://upload.wikimedia.org/wikipedia/en/4/47/Home_alone.jpg",
+                         "https://www.youtube.com/watch?v=CK2Btk6Ybm0")
+                         
+dazed_confused = media.Movie("Dazed and Confused",
+                             "1993",
+                             "The adventures of incoming high school and junior high students on the last day of school, in May of 1976.",
+                             "https://upload.wikimedia.org/wikipedia/en/9/9a/DazedConfused.jpg",
+                             "https://www.youtube.com/watch?v=3aQuvPlcB-8")   
+
+toy_story = media.Movie("Toy Story",
+                        "1995",
+                        "A story of a boy and his toys that come to life.",
+                        "https://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg",
+                        "https://www.youtube.com/watch?v=KYz2wyBy3kc")
+
+heavyweights = media.Movie("Heavyweights",
+                           "1995",
+                           "Plump kids are lured into joining a posh fat camp with the promise of quick weight loss and good times, only to find that the facility is a woodland hellhole run by a psycho ex-fitness instructor.",
+                           "https://upload.wikimedia.org/wikipedia/en/b/b0/Heavyweights-theatrical.jpg",
+                           "https://www.youtube.com/watch?v=uD4GclR6ZEo")
+
+superbad = media.Movie("Superbad",
+                       "2007",
+                       "Two co-dependent high school seniors are forced to deal with separation anxiety after their plan to stage a booze-soaked party goes awry.",
+                       "https://upload.wikimedia.org/wikipedia/en/8/8b/Superbad_Poster.png",
+                       "https://www.youtube.com/watch?v=MNpoTxeydiY")  
+
+#Create list of all Movie instances
+movies = [goodfellas, usual_suspects, django_unchained, reservoir_dogs, home_alone, dazed_confused, toy_story, heavyweights, superbad]
+
+#Generate movies web page and open in browser
+fresh_tomatoes.open_movies_page(movies)
