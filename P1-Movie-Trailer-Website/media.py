6e4c3ceb2bd138ae8cf910d3268af748caec7b9a
mrchristmas <mrchristmas@users.noreply.github.com>
2015-11-16 20:01:51 -0600

    Update media.py

--- P1-Movie-Trailer-Website/media.py
+++ P1-Movie-Trailer-Website/media.py
@@ -1,14 +1,15 @@
 import webbrowser
 
-class Movie():
-    """ This class provides a way to store movie information. """    
-         
+
+class Movie(object):
+    """ This class provides a way to store movie information. """
+
     def __init__(self, movie_title, year, movie_storyline, poster_image, trailer_youtube):
         self.title = movie_title
         self.year = year
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
-        self.trailer_youtube_url = trailer_youtube            
-        
+        self.trailer_youtube_url = trailer_youtube
+
     def show_trailer(self):
         webbrowser.open(self.trailer_youtube_url)
