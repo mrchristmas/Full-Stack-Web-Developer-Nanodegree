5ef0d53281e2005ebca118e3287ec6e5d0008038
mrchristmas <michaelschmitz5@gmail.com>
2016-01-25 22:43:36 -0600

    Turn off debug mode

--- P3-Item-Catalog/menuApp/app.py
+++ P3-Item-Catalog/menuApp/app.py
@@ -498,5 +498,5 @@ def deleteMenuItem(restaurant_id, menu_id):
 
 if __name__ == '__main__':
     app.secret_key = 'super_secret_key'
-    app.debug = True
+    app.debug = False
     app.run(host='0.0.0.0', port=5000)
